651af9670589d010860f20bb05968a909412c76e
Radovan Tkac <tkac.radovan@gmail.com>
2026-07-29 15:51:25 +0200

    feat: added default workspace creation

--- apps/admin-web/src/functions/workspace.functions.ts
+++ apps/admin-web/src/functions/workspace.functions.ts
@@ -1,8 +1,13 @@
 import { ensureSession } from '@feedback-saas/auth/functions/auth.functions';
-import { getUserWorkspaces } from '@feedback-saas/db/queries/workspace';
+import { createDefaultWorkspace, getUserWorkspaces } from '@feedback-saas/db/queries/workspace';
 import { createServerFn } from '@tanstack/react-start';
 
 export const getUserWorkspacesFn = createServerFn({ method: 'GET' }).handler(async () => {
   const session = await ensureSession();
-  return getUserWorkspaces(session.user.id);
+  const workspaces = await getUserWorkspaces(session.user.id);
+  if (workspaces.length === 0) {
+    const newWorkspace = await createDefaultWorkspace(session.user.id, session.user.name);
+    return newWorkspace;
+  }
+  return workspaces;
 });
