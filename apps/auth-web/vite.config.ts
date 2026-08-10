27e083bd7d4215748a6409df6a56da5eb02b38e5
Radovan Tkac <tkac.radovan@gmail.com>
2026-08-10 18:33:10 +0200

    fix: added nitro for vercel deployment

--- apps/auth-web/vite.config.ts
+++ apps/auth-web/vite.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from '@tailwindcss/vite';
 import { devtools } from '@tanstack/devtools-vite';
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import viteReact from '@vitejs/plugin-react';
+import { nitro } from 'nitro/vite';
 import { defineConfig } from 'vite';
 
 const config = defineConfig({
@@ -13,6 +14,7 @@ const config = defineConfig({
     devtools(),
     tailwindcss(),
     tanstackStart(),
+    nitro(),
     viteReact(),
   ],
 });
